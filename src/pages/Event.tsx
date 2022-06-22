064dc427f9c4f34ba5a471767dba81def8b8814d
tallismgf <tallismgf@hotmail.com>
2022-06-22 00:47:29 -0300

    feat: finish sidebar component and other configs

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,15 @@
-import { Header } from "../components";
+import { Header, Sidebar, Video } from "../components";
 
 export function Event() {
   return (
-    <Header />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main
+        className="flex flex-1"
+      >
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
   )
 }
