f2da031a73a72733b198d9eac0423191ef06f9b9
tallismgf <tallismgf@hotmail.com>
2022-06-23 00:52:27 -0300

    feat: add video components and routing

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -12,7 +13,8 @@ interface LessonProps {
 export function Lesson ({
   title,
   availableAt,
-  type
+  type,
+  slug
 }: LessonProps) {
   const isLessonAvailable = isPast(availableAt)
   const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
@@ -20,12 +22,12 @@ export function Lesson ({
   })
 
   return (
-    <a href="">
+    <Link to={`/event/lesson/${slug}`} className='group'>
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between" >
           {
             isLessonAvailable ? (
@@ -51,6 +53,6 @@ export function Lesson ({
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
